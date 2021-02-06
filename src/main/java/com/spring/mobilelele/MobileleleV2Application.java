f808e2812f6c177503a3b8b5dfc15e6901484240
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2021-02-06 15:46:24 +0200

    initial commit

---
+++ src/main/java/com/spring/mobilelele/MobileleleV2Application.java
@@ -0,0 +1,13 @@
+package com.spring.mobilelele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileleleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileleleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/mobilelele/MobileleleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mobilelele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
